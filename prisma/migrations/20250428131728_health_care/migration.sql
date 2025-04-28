7a59ac4b824ed2b7fff5cb32e87743d299122c92
Joy43 <ssjoy43@gmail.com>
2025-04-28 19:25:39 +0600

    feat: add patient health data and medical report models with relationships in Prisma schema

--- prisma/migrations/20250428131728_health_care/migration.sql
+++ prisma/migrations/20250428131728_health_care/migration.sql
@@ -7,6 +7,12 @@ CREATE TYPE "UserStatus" AS ENUM ('ACTIVE', 'BLOCKED', 'DELETED');
 -- CreateEnum
 CREATE TYPE "Gender" AS ENUM ('MALE', 'FEMALE');
 
+-- CreateEnum
+CREATE TYPE "BloodGroup" AS ENUM ('A_POSITIVE', 'B_POSITIVE', 'O_POSITIVE', 'AB_POSITIVE', 'A_NEGATIVE', 'B_NEGATIVE', 'O_NEGATIVE', 'AB_NEGATIVE');
+
+-- CreateEnum
+CREATE TYPE "MaritalStatus" AS ENUM ('MARRIED', 'UNMARRIED');
+
 -- CreateTable
 CREATE TABLE "users" (
     "id" TEXT NOT NULL,
@@ -89,6 +95,65 @@ CREATE TABLE "doctor_specialties" (
     CONSTRAINT "doctor_specialties_pkey" PRIMARY KEY ("specialitiesId","doctorId")
 );
 
+-- CreateTable
+CREATE TABLE "patient_health_datas" (
+    "id" TEXT NOT NULL,
+    "patientId" TEXT NOT NULL,
+    "gender" "Gender" NOT NULL,
+    "dateOfBirth" TEXT NOT NULL,
+    "bloodGroup" "BloodGroup" NOT NULL,
+    "hasAllergies" BOOLEAN DEFAULT false,
+    "hasDiabetes" BOOLEAN DEFAULT false,
+    "height" TEXT NOT NULL,
+    "weight" TEXT NOT NULL,
+    "smokingStatus" BOOLEAN DEFAULT false,
+    "dietaryPreferences" TEXT,
+    "pregnancyStatus" BOOLEAN DEFAULT false,
+    "mentalHealthHistory" TEXT,
+    "immunizationStatus" TEXT,
+    "hasPastSurgeries" BOOLEAN DEFAULT false,
+    "recentAnxiety" BOOLEAN DEFAULT false,
+    "recentDepression" BOOLEAN DEFAULT false,
+    "maritalStatus" "MaritalStatus" NOT NULL DEFAULT 'UNMARRIED',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "patient_health_datas_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "madical_reports" (
+    "id" TEXT NOT NULL,
+    "patientId" TEXT NOT NULL,
+    "reportName" TEXT NOT NULL,
+    "reportLink" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "madical_reports_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "schedules" (
+    "id" TEXT NOT NULL,
+    "startDateTime" TIMESTAMP(3) NOT NULL,
+    "endDateTime" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "schedules_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "doctor_schedules" (
+    "doctorId" TEXT NOT NULL,
+    "scheduleId" TEXT NOT NULL,
+    "isBooked" BOOLEAN NOT NULL,
+    "appointmentId" TEXT,
+
+    CONSTRAINT "doctor_schedules_pkey" PRIMARY KEY ("doctorId","scheduleId")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
 
@@ -104,6 +169,9 @@ CREATE UNIQUE INDEX "patients_id_key" ON "patients"("id");
 -- CreateIndex
 CREATE UNIQUE INDEX "patients_email_key" ON "patients"("email");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "patient_health_datas_patientId_key" ON "patient_health_datas"("patientId");
+
 -- AddForeignKey
 ALTER TABLE "admins" ADD CONSTRAINT "admins_email_fkey" FOREIGN KEY ("email") REFERENCES "users"("email") ON DELETE RESTRICT ON UPDATE CASCADE;
 
@@ -118,3 +186,15 @@ ALTER TABLE "doctor_specialties" ADD CONSTRAINT "doctor_specialties_specialities
 
 -- AddForeignKey
 ALTER TABLE "doctor_specialties" ADD CONSTRAINT "doctor_specialties_doctorId_fkey" FOREIGN KEY ("doctorId") REFERENCES "doctors"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "patient_health_datas" ADD CONSTRAINT "patient_health_datas_patientId_fkey" FOREIGN KEY ("patientId") REFERENCES "patients"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "madical_reports" ADD CONSTRAINT "madical_reports_patientId_fkey" FOREIGN KEY ("patientId") REFERENCES "patients"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "doctor_schedules" ADD CONSTRAINT "doctor_schedules_doctorId_fkey" FOREIGN KEY ("doctorId") REFERENCES "doctors"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "doctor_schedules" ADD CONSTRAINT "doctor_schedules_scheduleId_fkey" FOREIGN KEY ("scheduleId") REFERENCES "schedules"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
